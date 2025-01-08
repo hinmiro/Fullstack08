83d488e80fc20ed33678a8be3645a2406207adb1
mirohi <miro.hintikka@metropolia.fi>
2025-01-09 00:00:54 +0200

    Add assignment 8.13 and start 8.14

---
+++ library-backend/src/mongoSchema/authorSchema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const uniqueValidator = require('mongoose-unique-validator')
+
+const schema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+            minlength: 4,
+        },
+        born: {
+            type: Number,
+        },
+    },
+    { collection: 'author' }
+)
+
+schema.plugin(uniqueValidator)
+
+module.exports = mongoose.model('author', schema)
